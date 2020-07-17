cb8d2cb77403f8be155056c3c246c28bb20d7db2
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-16 21:59:08 -0300

    indent with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 const index = () => {
-  return (
-    <div>
-      <h1>My new next app!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>My new next app!</h1>
+		</div>
+	);
 };
 
 export default index;
