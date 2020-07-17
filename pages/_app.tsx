a2b4f4533fa7033dbe45b977cd49d161e1152b28
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-16 21:59:02 -0300

    indent with prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,24 +3,24 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <Head>
-        <title>Your good title!</title>
+	return (
+		<div>
+			<Head>
+				<title>Your good title!</title>
 
-        {/* font-family */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </div>
-  );
+				{/* font-family */}
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;900&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
 export default MyApp;
