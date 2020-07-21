4d15fd40e090cd1e4c945f0e199a024e5b3de376
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 17:45:17 -0300

    :tada: add animation to contact-data

--- components/Contact/ContactData.tsx
+++ components/Contact/ContactData.tsx
@@ -1,18 +1,21 @@
 import style from './style.module.scss';
+import Fade from 'react-reveal/Fade';
 
 const Contact: React.FC = () => {
 	return (
-		<div className={style.contact}>
-			<div className={style.email}>
-				<p className={style.title}>Need help?</p>
-				<span className={style.data}>company@example.com</span>
-			</div>
+		<Fade left cascade>
+			<div className={style.contact}>
+				<div className={style.email}>
+					<p className={style.title}>Need help?</p>
+					<span className={style.data}>company@example.com</span>
+				</div>
 
-			<div className={style.phone}>
-				<p className={style.title}>Write down our phone</p>
-				<span className={style.data}>+55 (11) 4002-8922</span>
+				<div className={style.phone}>
+					<p className={style.title}>Write down our phone</p>
+					<span className={style.data}>+55 (11) 4002-8922</span>
+				</div>
 			</div>
-		</div>
+		</Fade>
 	);
 };
 
