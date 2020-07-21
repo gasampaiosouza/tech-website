c824c9eb7024de38a782e64322711d618c6a2a54
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 17:45:37 -0300

    :tada: add animation to message

--- components/Contact/Message.tsx
+++ components/Contact/Message.tsx
@@ -1,18 +1,21 @@
 import style from './style.module.scss';
+import Fade from 'react-reveal/Fade';
 
 const Contact: React.FC = () => {
 	return (
-		<div className={style.message}>
-			<h1 className={`${style['message-title']} title`}>
-				Be in touch sending us a <span>message</span>!
-			</h1>
+		<Fade right cascade>
+			<div className={style.message}>
+				<h1 className={`${style['message-title']} title`}>
+					Be in touch sending us a <span>message</span>!
+				</h1>
 
-			<p className={style['message-description']}>
-				You can send us <span>troubles</span> you are having with your
-				electronics, <span>suggestions</span>, <span>criticism</span> or{' '}
-				<span>compliments</span>. Feel free to send us any type of message!
-			</p>
-		</div>
+				<p className={style['message-description']}>
+					You can send us <span>troubles</span> you are having with your
+					electronics, <span>suggestions</span>, <span>criticism</span> or{' '}
+					<span>compliments</span>. Feel free to send us any type of message!
+				</p>
+			</div>
+		</Fade>
 	);
 };
 
