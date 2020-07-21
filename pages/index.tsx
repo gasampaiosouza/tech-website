df069456a3aefa968b139a0a14cc40dfe0696082
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 01:06:25 -0300

    add services & equipments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Header from '../components/Header';
+import Presentation from '../components/Presentation';
+import Services from '../components/Services';
+import Equipments from '../components/Equipments';
 
 const index = () => {
 	return (
 		<main>
 			<Header />
+			<Presentation />
+			<Services />
+			<Equipments />
 		</main>
 	);
 };
