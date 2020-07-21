a5310e207305bc2dd5330de66e734e094e75f67c
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 14:44:29 -0300

    add contact-data section

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -9,7 +9,17 @@ const Contact: React.FC = () => {
 			<div className={style.content}>
 				<OpeningHours />
 				<div className={style.message}>message</div>
-				<div className={style.contato}>contato</div>
+				<div className={style.contact}>
+					<div className={style.email}>
+						<p className={style.title}>Precisa de ajuda?</p>
+						<span className={style.data}>supertechbsb@gmail.com</span>
+					</div>
+
+					<div className={style.phone}>
+						<p className={style.title}>Anote nosso telefone</p>
+						<span className={style.data}>(61) 99591-0125</span>
+					</div>
+				</div>
 				<Form />
 			</div>
 		</section>
