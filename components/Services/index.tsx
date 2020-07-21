9e23b879374aedff5ceecc4d2eaa04d8dea0040d
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 17:34:36 -0300

    :tada: add animations to services

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -1,26 +1,30 @@
 import style from './style.module.scss';
-// https://imgur.com/a/6aIdKV0
+import Fade from 'react-reveal/Fade';
 
 const Services: React.FC = () => {
 	return (
 		<div className={style.container}>
-			<img
-				src="https://imgur.com/8eYakLA.png"
-				className="icon"
-				alt="services"
-			/>
+			<Fade left>
+				<img
+					src="https://imgur.com/8eYakLA.png"
+					className="icon"
+					alt="services"
+				/>
+			</Fade>
 
-			<div className={style.content}>
-				<h1 className="title">Company services</h1>
+			<Fade right cascade>
+				<div className={style.content}>
+					<h1 className="title">Company services</h1>
 
-				<p className={`description ${style.description}`}>
-					We from Company work hard to solve the variety of our customers
-					problems. Like <span>maintence</span>, <span>formatting</span> and{' '}
-					<span>cleaning</span> your <span>electronics</span>, we help you with{' '}
-					<span>xerox</span> and you’re always welcome to our{' '}
-					<span>Lan House</span>!
-				</p>
-			</div>
+					<p className={`description ${style.description}`}>
+						We from Company work hard to solve the variety of our customers
+						problems. Like <span>maintence</span>, <span>formatting</span> and{' '}
+						<span>cleaning</span> your <span>electronics</span>, we help you
+						with <span>xerox</span> and you’re always welcome to our{' '}
+						<span>Lan House</span>!
+					</p>
+				</div>
+			</Fade>
 		</div>
 	);
 };
