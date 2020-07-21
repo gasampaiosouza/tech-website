be007e26e5640fa8b8c5a47fe446700f36772015
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 17:34:26 -0300

    :tada: add animations to equipment

--- components/Equipments/index.tsx
+++ components/Equipments/index.tsx
@@ -1,23 +1,28 @@
 import style from './style.module.scss';
+import Fade from 'react-reveal/Fade';
 
 const Equipments: React.FC = () => {
 	return (
 		<div className={style.container}>
-			<img
-				src="https://imgur.com/fgwEyqs.png"
-				className="icon"
-				alt="equipments"
-			/>
+			<Fade right>
+				<img
+					src="https://imgur.com/fgwEyqs.png"
+					className="icon"
+					alt="equipments"
+				/>
+			</Fade>
 
-			<div className={style.content}>
-				<h1 className="title">Our equipments</h1>
+			<Fade left cascade>
+				<div className={style.content}>
+					<h1 className="title">Our equipments</h1>
 
-				<p className={`description ${style.description}`}>
-					We work with any type of equipment, including the modern ones! Like
-					<span>desktop</span>, <span>notebooks</span>, <span>printers</span>{' '}
-					and <span>video-games</span>!
-				</p>
-			</div>
+					<p className={`description ${style.description}`}>
+						We work with any type of equipment, including the modern ones! Like
+						<span>desktop</span>, <span>notebooks</span>, <span>printers</span>{' '}
+						and <span>video-games</span>!
+					</p>
+				</div>
+			</Fade>
 		</div>
 	);
 };
