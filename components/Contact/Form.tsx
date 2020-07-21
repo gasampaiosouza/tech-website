d9bee45dc7d6b731c2355572f6b20dd97d4da0bc
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 15:40:06 -0300

    finish form

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -3,8 +3,24 @@ import style from './style.module.scss';
 const Contact: React.FC = () => {
 	return (
 		<form className={style.form}>
-			<p>form</p>
-			<p>ulario</p>
+			<div className={style['form-name']}>
+				<label htmlFor="name">Nome</label>
+				<input
+					className={style.input}
+					type="text"
+					id="name"
+					required
+					maxLength={35}
+				/>
+			</div>
+
+			<textarea
+				placeholder="Envie sua mensagem aqui..."
+				className={style.input}
+				required
+			></textarea>
+
+			<button className="btn">Enviar</button>
 		</form>
 	);
 };
