2c1aa9a33631eaa97d3535c012ac05248da8d2b7
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 15:09:32 -0300

    add final sections

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,25 +1,17 @@
 import style from './style.module.scss';
 
 import OpeningHours from './OpeningHours';
+import ContactData from './ContactData';
 import Form from './Form';
+import Message from './Message';
 
 const Contact: React.FC = () => {
 	return (
 		<section className={style.container}>
 			<div className={style.content}>
 				<OpeningHours />
-				<div className={style.message}>message</div>
-				<div className={style.contact}>
-					<div className={style.email}>
-						<p className={style.title}>Precisa de ajuda?</p>
-						<span className={style.data}>supertechbsb@gmail.com</span>
-					</div>
-
-					<div className={style.phone}>
-						<p className={style.title}>Anote nosso telefone</p>
-						<span className={style.data}>(61) 99591-0125</span>
-					</div>
-				</div>
+				<Message />
+				<ContactData />
 				<Form />
 			</div>
 		</section>
