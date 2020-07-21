72fd0a41087c71dc1854e4adb37a9f4ef5386d80
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 16:16:15 -0300

    change presentation text to english

--- components/Presentation/index.tsx
+++ components/Presentation/index.tsx
@@ -3,16 +3,16 @@ import style from './style.module.scss';
 const Presentation: React.FC = () => {
 	return (
 		<div className={style.container}>
-			<h1 className="title">Bem-vindo à SuperTech!</h1>
+			<h1 className="title">Welcome to Company!</h1>
 
 			<p className={`description ${style.description}`}>
-				A SuperTech é uma empresa de informática que te ajuda em muuuuita coisa,
-				como <span>manutenções</span>, <span>formatações</span>,{' '}
-				<span>limpezas</span>, e <span>xerox</span>. Tentando sempre te ajudar
-				com o que você precisar!
+				Company is a tech company that helps you in a looot of things, like
+				<span>maintence</span>, <span>formatting</span>, <span>cleanings</span>,
+				and <span>xerox</span>. Always trying to help you with anything you
+				need!
 			</p>
 
-			<button className="btn">Tenho interesse</button>
+			<button className="btn">I am interested</button>
 		</div>
 	);
 };
