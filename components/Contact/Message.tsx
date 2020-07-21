aae407165d6509d17da98cd4eac556b26f3c8b12
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 14:35:24 -0300

    add message boilerplate

---
+++ components/Contact/Message.tsx
@@ -0,0 +1,11 @@
+import style from './style.module.scss';
+
+const Contact: React.FC = () => {
+	return (
+		<div className={style.message}>
+			<h1>Message</h1>
+		</div>
+	);
+};
+
+export default Contact;
