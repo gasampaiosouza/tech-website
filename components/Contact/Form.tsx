755b736970a08de8c689a25bb084557dc8017e5a
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 17:45:25 -0300

    :tada: add animation to form

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -1,27 +1,30 @@
 import style from './style.module.scss';
+import Fade from 'react-reveal/Fade';
 
 const Contact: React.FC = () => {
 	return (
-		<form className={style.form}>
-			<div className={style['form-name']}>
-				<label htmlFor="name">Name</label>
-				<input
+		<Fade right cascade>
+			<form className={style.form}>
+				<div className={style['form-name']}>
+					<label htmlFor="name">Name</label>
+					<input
+						className={style.input}
+						type="text"
+						id="name"
+						required
+						maxLength={35}
+					/>
+				</div>
+
+				<textarea
+					placeholder="Write your message here..."
 					className={style.input}
-					type="text"
-					id="name"
 					required
-					maxLength={35}
-				/>
-			</div>
+				></textarea>
 
-			<textarea
-				placeholder="Write your message here..."
-				className={style.input}
-				required
-			></textarea>
-
-			<button className="btn">Send</button>
-		</form>
+				<button className="btn">Send</button>
+			</form>
+		</Fade>
 	);
 };
 
