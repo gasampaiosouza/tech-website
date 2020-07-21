b5a6061e951b568d15bc122d04a6fe26651c60a6
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 15:10:00 -0300

    finish message section

--- components/Contact/Message.tsx
+++ components/Contact/Message.tsx
@@ -3,7 +3,15 @@ import style from './style.module.scss';
 const Contact: React.FC = () => {
 	return (
 		<div className={style.message}>
-			<h1>Message</h1>
+			<h1 className={`${style['message-title']} title`}>
+				Entre em contato nos enviando uma <span>mensagem</span>!
+			</h1>
+
+			<p className={style['message-description']}>
+				Você pode nos enviar <span>problemas</span> que você tendo com algum de
+				seus eletrônicos, <span>sugestões</span>, <span>críticas</span> ou{' '}
+				<span>elogios</span>. Sinta-se livre para me mandar qualquer mensagem!
+			</p>
 		</div>
 	);
 };
