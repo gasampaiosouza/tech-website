c1ba8c1f6fe708050ef48113f29a43c5e1bff9e4
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 17:51:10 -0300

    :tada: add animation to footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,63 +1,78 @@
 import style from './style.module.scss';
+import Fade from 'react-reveal/Fade';
 
 const Footer: React.FC = () => {
 	return (
 		<footer className={style.container}>
 			<div className={`${style['footer-company']} ${style.section}`}>
-				<h1 className={`${style.title} ${style.logo} logo`}>Company</h1>
-				<p className={`${style.description} description`}>
-					This company is a tech focused company. We can help you with any
-					electronic equipment.
-				</p>
+				<Fade top>
+					<h1 className={`${style.title} ${style.logo} logo`}>Company</h1>
+				</Fade>
+				<Fade left>
+					<p className={`${style.description} description`}>
+						This company is a tech focused company. We can help you with any
+						electronic equipment.
+					</p>
+				</Fade>
 			</div>
 
 			<div className={`${style['footer-links']} ${style.section}`}>
-				<h1 className={`${style.title}`}>Links</h1>
+				<Fade top>
+					<h1 className={`${style.title}`}>Links</h1>
+				</Fade>
 				<ul>
-					<a href="#">
-						<li>Home</li>
-					</a>
-					<a href="#">
-						<li>Services</li>
-					</a>
-					<a href="#">
-						<li>Equipments</li>
-					</a>
-					<a href="#">
-						<li>Opening hours</li>
-					</a>
-					<a href="#">
-						<li>Contact</li>
-					</a>
+					<Fade bottom cascade>
+						<a href="#">
+							<li>Home</li>
+						</a>
+						<a href="#">
+							<li>Services</li>
+						</a>
+						<a href="#">
+							<li>Equipments</li>
+						</a>
+						<a href="#">
+							<li>Opening hours</li>
+						</a>
+						<a href="#">
+							<li>Contact</li>
+						</a>
+					</Fade>
 				</ul>
 			</div>
 
 			<div className={`${style['footer-contact']} ${style.section}`}>
-				<h1 className={`${style.title}`}>Contact</h1>
-				<ul>
-					<li>
-						<i className={`fas fa-map-marker-alt ${style.icon}`}></i>
-						<p>Glory Street - LA</p>
-					</li>
+				<Fade top>
+					<h1 className={`${style.title}`}>Contact</h1>
+				</Fade>
+				<Fade left cascade>
+					<ul>
+						<li>
+							<i className={`fas fa-map-marker-alt ${style.icon}`}></i>
+							<p>Glory Street - LA</p>
+						</li>
 
-					<li>
-						<i className={`fas fa-envelope ${style.icon}`}></i>
-						<p>company@example.com</p>
-					</li>
+						<li>
+							<i className={`fas fa-envelope ${style.icon}`}></i>
+							<p>company@example.com</p>
+						</li>
 
-					<li>
-						<i className={`fab fa-instagram ${style.icon}`}></i>
-						<p>@coolcompany</p>
-					</li>
+						<li>
+							<i className={`fab fa-instagram ${style.icon}`}></i>
+							<p>@coolcompany</p>
+						</li>
 
-					<li>
-						<i className={`fas fa-phone-alt ${style.icon}`}></i>
-						<p>+55 (11) 4002-8922</p>
-					</li>
-				</ul>
+						<li>
+							<i className={`fas fa-phone-alt ${style.icon}`}></i>
+							<p>+55 (11) 4002-8922</p>
+						</li>
+					</ul>
+				</Fade>
 			</div>
 
-			<p className={style.copyright}>©2020 Copyright: Company.com</p>
+			<Fade bottom>
+				<p className={style.copyright}>©2020 Copyright: Company.com</p>
+			</Fade>
 		</footer>
 	);
 };
