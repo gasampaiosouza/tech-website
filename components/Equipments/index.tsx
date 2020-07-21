1fadf4515f723394a836a169720df907f59f0d87
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-21 14:33:34 -0300

    remove comment

--- components/Equipments/index.tsx
+++ components/Equipments/index.tsx
@@ -1,7 +1,23 @@
+import style from './style.module.scss';
+// https://imgur.com/a/6aIdKV0
+
 const Equipments: React.FC = () => {
 	return (
-		<div>
-			<h1>Equipments</h1>
+		<div className={style.container}>
+			<img
+				src="https://imgur.com/fgwEyqs.png"
+				className="icon"
+				alt="equipments"
+			/>
+
+			<div className={style.content}>
+				<h1 className="title">Nossos equipamentos</h1>
+
+				<p className={`description ${style.description}`}>
+					Trabalhamos com todos os tipos de equipamentos, incluindo os mais
+					modernos! Como desktop, notebooks, impressoras e video-games!
+				</p>
+			</div>
 		</div>
 	);
 };
